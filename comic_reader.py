5c925ebb0db68226c4bc744af91d7dc2ab260440
Purnesh Dixit <pudixit@microsoft.com>
2019-07-22 21:05:48 -0700

    text to speech successfull

--- comic_reader.py
+++ comic_reader.py
@@ -19,7 +19,7 @@ class TextToSpeech(object):
         self.access_token = None
     
     def get_token(self):
-        fetch_token_url = "https://westus2.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1"
+        fetch_token_url = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken"
         headers = {
             'Ocp-Apim-Subscription-Key': self.subscription_key
         }
@@ -27,14 +27,14 @@ class TextToSpeech(object):
         self.access_token = str(response.text)
 
     def save_audio(self):
-        base_url = 'https://westus2.tts.speech.microsoft.com/cognitiveservices/v1'
+        base_url = 'https://westus.tts.speech.microsoft.com/'
         path = 'cognitiveservices/v1'
         constructed_url = base_url + path
         headers = {
             'Authorization': 'Bearer ' + self.access_token,
             'Content-Type': 'application/ssml+xml',
             'X-Microsoft-OutputFormat': 'riff-24khz-16bit-mono-pcm',
-            'User-Agent': 'comic-reader'
+            'User-Agent': 'Text-to-Speech'
         }
         xml_body = ElementTree.Element('speak', version='1.0')
         xml_body.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-us')
@@ -52,14 +52,15 @@ class TextToSpeech(object):
                 print("\nStatus code: " + str(response.status_code) +
                     "\nYour TTS is ready for playback.\n")
         else:
+            print(response.content)
             print("\nStatus code: " + str(response.status_code) +
                 "\nSomething went wrong. Check your subscription key and headers.\n")
 
 
 if __name__ == "__main__":
-    subscription_key = "26c9ead4f6294f87b886879be7d718d7"
+    subscription_key = ""
     app = TextToSpeech(subscription_key)
     app.get_token()
     app.save_audio()
 
-print(ImageDescriber.get_image_description('Images/Animals/Monkey/Test/monkey_with_dog.JPG'))
+#print(ImageDescriber.get_image_description('Images/Animals/Monkey/Test/monkey_with_dog.JPG'))
