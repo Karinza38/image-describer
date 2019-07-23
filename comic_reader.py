f586bd89a52731783af46e877d608e2594279ec5
Purnesh Dixit <pudixit@microsoft.com>
2019-07-22 20:29:47 -0700

    comic image mock successfull

--- comic_reader.py
+++ comic_reader.py
@@ -1,3 +1,65 @@
 import image_describer as ImageDescriber
 
+import os
+import requests
+import time
+from xml.etree import ElementTree
+
+try:
+    input = raw_input
+except NameError:
+    pass
+
+
+class TextToSpeech(object):
+    def __init__(self, subscription_key):
+        self.subscription_key = subscription_key
+        self.tts = input("What would you like to convert to speech: ")
+        self.timestr = time.strftime("%Y%m%d-%H%M")
+        self.access_token = None
+    
+    def get_token(self):
+        fetch_token_url = "https://westus2.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1"
+        headers = {
+            'Ocp-Apim-Subscription-Key': self.subscription_key
+        }
+        response = requests.post(fetch_token_url, headers=headers)
+        self.access_token = str(response.text)
+
+    def save_audio(self):
+        base_url = 'https://westus2.tts.speech.microsoft.com/cognitiveservices/v1'
+        path = 'cognitiveservices/v1'
+        constructed_url = base_url + path
+        headers = {
+            'Authorization': 'Bearer ' + self.access_token,
+            'Content-Type': 'application/ssml+xml',
+            'X-Microsoft-OutputFormat': 'riff-24khz-16bit-mono-pcm',
+            'User-Agent': 'comic-reader'
+        }
+        xml_body = ElementTree.Element('speak', version='1.0')
+        xml_body.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-us')
+        voice = ElementTree.SubElement(xml_body, 'voice')
+        voice.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-US')
+        voice.set(
+            'name', 'Microsoft Server Speech Text to Speech Voice (en-US, Guy24KRUS)')
+        voice.text = self.tts
+        body = ElementTree.tostring(xml_body)
+
+        response = requests.post(constructed_url, headers=headers, data=body)
+        if response.status_code == 200:
+            with open('sample-' + self.timestr + '.wav', 'wb') as audio:
+                audio.write(response.content)
+                print("\nStatus code: " + str(response.status_code) +
+                    "\nYour TTS is ready for playback.\n")
+        else:
+            print("\nStatus code: " + str(response.status_code) +
+                "\nSomething went wrong. Check your subscription key and headers.\n")
+
+
+if __name__ == "__main__":
+    subscription_key = "26c9ead4f6294f87b886879be7d718d7"
+    app = TextToSpeech(subscription_key)
+    app.get_token()
+    app.save_audio()
+
 print(ImageDescriber.get_image_description('Images/Animals/Monkey/Test/monkey_with_dog.JPG'))
--- image_describer.py
+++ image_describer.py
@@ -10,10 +10,10 @@ def get_image_data(image_path):
     return f
 
 def get_image_description(image_path):
-    url = 'https://westus2.api.cognitive.microsoft.com/customvision/v3.0/Prediction/610fd33e-e7c8-4429-a3ac-1dbedcbc3a2e/classify/iterations/image_describer/image'
+    url = ''
     image_bytes = get_image_data(image_path)
 
-    headers = {"Prediction-Key": "6a9ad0ebebf04268a21377194ff5eaa6",
+    headers = {"Prediction-Key": "",
                "Content-Type": "application/octet-stream"}
 
     r = requests.post(url, data=image_bytes, headers=headers)
